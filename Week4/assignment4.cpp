ef27a673a8775e999ea32668565947c26bdfd3a3
kelricalama <kelricalama@macbookair.mynetworksettings.com>
2026-07-08 07:05:16 -0400

    Update project

--- Week4/assignment4.cpp
+++ Week4/assignment4.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 
 void removeCopies(std::vector<int>& data){
 
-    // The vector is sorted in non-decreasing order.
-    // Remove all duplicate values in-place so that
-    // each value appears exactly once.
-    //
-    // Do not create another vector.
-    // Modify the vector named "data" directly.
-    //
-    // Maybe two-pointer method? Hmm?
-    // Hint: resize() function
+    if (data.empty())
+    return;
+    
+    int write = 0;
 
+    for (int read = 1; read < data.size(); read++) {
+        if (data[read] != data[write]) {
+            write++;
+            data[write] = data[read];
+        }
+    }
+    data.resize(write + 1);
 }
 
 void isAnagram(std::string officer_name, std::string spy_name){
+    
+    if (officer_name.length() != spy_name.length()) {
+        std::cout << "Imposter!" << std::endl;
+        return;
+    }
 
-    // Write code to see if the spy name is an anagram of the officer name 
-    // Maybe a vector of size 26? Hmm?
+    std::vector<int> letters(26, 0);
+
+    for (char c : officer_name) {
+        letters[c - 'a']++;
+    }
+
+    for (char c : spy_name) {
+        letters[c - 'a']--;
+    }
+
+    for (int count : letters) {
+        if (count != 0) {
+            std::cout << "Imposter!" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "No imposter found!" << std::endl;
 
 }
 
